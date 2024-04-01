163f3ecaa379c5e477012d05b101cc7c8e3c15fa
Tommy0901 <ak710180@gmail.com>
2024-04-01 15:53:44 +0800

    feat: update main, login and register page

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,8 @@ const port = 3000
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 for (const route of router) {
   app.use(route.getRouter())
 }
